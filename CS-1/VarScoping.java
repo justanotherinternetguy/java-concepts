416ce3f8f95ac8df7e6b40d89a5cd3f55e9475ff
justanotherinternetguy <96833060+justanotherinternetguy@users.noreply.github.com>
2022-03-19 09:51:19 -0400

    e

--- CS-1/VarScoping.java
+++ CS-1/VarScoping.java
@@ -20,3 +20,22 @@ public class VarScoping {
 	}
 
 }
+
+/*
+ * Access modifiers are keywords in java that change how
+ * a method / class / object / variable is accessed.
+ */
+
+/*
+ * default (no keyword) - all entities are usable within SAME PACKAGE
+ * public - ENTIRE WORLD can use it, even other packages and classes
+ * private - usable within SAME CLASS (encapsulation)!
+ * protected - usable within same package AND all subclasses
+ */
+
+/*
+ * public - CLASS, PACKAGE, SUBCLASS, WORLD
+ * protected - CLASS, PACKAGE, SUBCLASS
+ * default - CLASS, PACKAGE
+ * private - CLASS
+ */
