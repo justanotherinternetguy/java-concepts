42eee1e388e698ac030bf8439cfd660dd763a531
justanotherinternetguy <96833060+justanotherinternetguy@users.noreply.github.com>
2022-04-11 10:11:37 -0400

    Update Regex.java
    e

--- misc/Regex.java
+++ misc/Regex.java
@@ -2,6 +2,26 @@ import java.util.regex.*;
 
 class Main {
     public static void main (String[] args) {
+        Pattern pattern = Pattern.compile("hello world", Pattern.CASE_INSENSITIVE);
 
+        /* RegEx for FIRST PARAMETER of compile()
+         *
+         * [xyz] find one character in between []
+         * [^xyz] find one character NOT in between []
+         * [0-9] find character 0-9
+         *
+         * METACHARS:
+         * x|y|z
+
+
+        Matcher matcher = pattern.matcher("Say Hello World!");
+        boolean matchFound = matcher.find();
+        if (matchFound) {
+            System.out.println("match found");
+        }
+
+        else {
+            System.out.println("not found");
+        }
     }
 }
