2bf9c5db10eab33d56146b00f043afac2b069ba1
justanotherinternetguy <96833060+justanotherinternetguy@users.noreply.github.com>
2022-04-16 19:13:25 -0400

    Update BubbleSort.java

--- SortingAlgorithms/BubbleSort.java
+++ SortingAlgorithms/BubbleSort.java
@@ -1,27 +1,34 @@
 public class BubbleSort {
-	
-	static void BubbleSort (int[] arr) {
-		int n = arr.length;
-		
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < (n-1); j++) {
-				if (arr[j] > arr[i]) {
-					int temp = arr[i];
-					arr[i] = arr[j];
-					arr[j] = temp;
-				}
-			}
-		}
-	}
 
-	public static void main(String[] args) {
-		int[] arr = {9, 8, 7, 6, 5, 4, 3, 2, 1};
-		BubbleSort(arr);
-		
-		for (int i = 0; i < arr.length; i++) {
-			System.out.println(arr[i]);
-		}
+    static void bubbleSort (int[] arr) {
+        int size = arr.length;
 
-	}
+        // loop to access each array element
+        for (int i = 0; i < size - 1; i++)
 
+            // loop to compare array elements
+            for (int j = 0; j < size - i - 1; j++)
+
+                // compare two adjacent elements
+                // change > to < to sort in descending order
+                if (arr[j] > arr[j + 1]) {
+
+                    // swapping occurs if elements
+                    // are not in the intended order
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {7, 6, 5, 4, 3, 2, 1};
+        int n = arr.length;
+
+        bubbleSort(arr);
+
+        for (int i : arr) {
+            System.out.println(i);
+        }
+    }
 }
